41cf187e501cb68b9872f0fdc99926ed24447f87
Tudor Ilisoi <tudorilisoi@yahoo.com>
2018-05-19 19:52:45 +0300

    folder structure

---
+++ src/Store.js
@@ -0,0 +1,149 @@
+import _ from "lodash";
+import cuid from "cuid";
+import scheduler from './timeout-scheduler.js'
+
+function splitPath(pathStr) {
+    return pathStr.split('.')
+}
+
+function splitProp(pathStr) {
+    const segments = splitPath(pathStr)
+    let prop = segments.pop()
+    return {
+        segments,
+        prop,
+    }
+}
+
+function mutate(val) {
+    return _.isArray(val) ? val.slice(0) :
+        _.isObject(val) ? Object.assign({}, val) : val
+}
+
+class Store {
+    constructor(initial = {}, opts = {}) {
+
+        this.jso = _.cloneDeep(initial)
+
+        this.opts = opts
+        this.defaults = initial
+        this.subscriptionsByPath = {}
+        this.subscribers = {}
+    }
+
+    get(pathStr) {
+        if (!pathStr) {
+            return this.jso
+        }
+        if (_.isArray(pathStr)) {
+            let ret = {}
+            pathStr.forEach(_pathStr => ret[_pathStr] = this.get(_pathStr))
+            return ret
+        }
+        return this.getSegment(pathStr).value()
+    }
+
+    set(pathStr, value) {
+        const s = this.getSegment(pathStr)
+        const prev = s.value()
+        if (prev === value) {
+            console.log('SET: NOOP', pathStr, value);
+            return
+        }
+        console.log('SET', pathStr, prev, value, s);
+        s.set(value)
+        const {segments} = splitProp(pathStr)
+        let changedPath = []
+        segments.forEach(segmentStr => {
+            changedPath.push(segmentStr)
+            const p = changedPath.join('.')
+            const so = this.getSegment(p)
+            console.log('MUTATE PARENT', p);
+            so.set(mutate(so.value()))
+            this.notify(p)
+        })
+        this.notify(pathStr)
+    }
+
+    addPath(pathStr, initialValue) {
+        const segments = splitPath(pathStr)
+        const l = segments.length
+        let s = this.jso
+        segments.forEach((segment, idx) => {
+            if (s[segment] === undefined) {
+                s[segment] = idx === l - 1 ? initialValue : {}
+            }
+            s = s[segment]
+        })
+        console.log('STORE', this.jso);
+    }
+
+    unsubscribe(uid) {
+        const {subscriptionsByPath, subscribers} = this
+        const sub = subscribers[uid]
+        delete subscribers[uid]
+        for (pathStr of sub.pathsArr) {
+            console.log('UNSUB', uid, pathStr);
+            let index = subscriptionsByPath[pathStr].indexOf(sub)
+            if (index > -1) {
+                subscriptionsByPath[pathStr].splice(index, 1)
+            }
+        }
+    }
+
+    subscribe(pathsArr, callback, opts = {}) {
+        const uid = cuid()
+        const sub = {
+            uid,
+            pathsArr,
+            callback,
+            opts
+        }
+        this.subscribers[uid] = sub
+        const {subscriptionsByPath} = this
+        pathsArr.forEach(pathStr => {
+            subscriptionsByPath[pathStr] = subscriptionsByPath[pathStr] || []
+            subscriptionsByPath[pathStr].push(sub)
+        })
+        return this.unsubscribe.bind(this, uid)
+    }
+
+    notify(pathStr) {
+        const {subscriptionsByPath} = this
+        const subs = subscriptionsByPath[pathStr]
+        if (subs === undefined) {
+            return
+        }
+        scheduler.queue(() => {
+            subs.forEach(sub => {
+                const cbValue = this.get(sub.pathsArr)
+                sub.callback(cbValue)
+                console.log('NOTIFY', pathStr, sub.uid, cbValue);
+            })
+
+        })
+    }
+
+    getParent(pathStr) {
+        const {segments} = splitProp(pathStr)
+        let parent = this.jso
+        segments.forEach(segment => {
+            parent = parent[segment]
+            if (parent === undefined) {
+                throw new Error(`STORE: NO SUCH PATH '${pathStr}', segment '${segment}'`)
+            }
+        })
+        return parent
+    }
+
+    getSegment(pathStr) {
+        const {prop} = splitProp(pathStr)
+        const ret = {
+            value: () => this.getParent(pathStr)[prop],
+            set: (value) => this.getParent(pathStr)[prop] = value,
+        }
+        return ret
+    }
+}
+
+export default Store
---
+++ src/connect.js
@@ -0,0 +1,175 @@
+import _ from "lodash";
+import React from "react";
+import scheduler from './timeout-scheduler.js'
+
+
+function _getPropName(map) {
+    if (Array.isArray(map)) {
+        return map[0]
+    } else {
+        return map
+    }
+}
+
+function _transformProp(map, value, valuesMap) {
+    if (Array.isArray(map)) {
+        const t = map[1]
+        return t(value, valuesMap)
+    } else {
+        return value
+    }
+}
+
+function _mapProps(valuesMap, mutations, objMap) {
+    const newState = {}
+    _.each(valuesMap, (val, storePath) => {
+        const map = objMap[storePath]
+        let propName = _getPropName(map)
+        if (propName === '_') {
+            propName = storePath
+        }
+        if (propName !== null) {
+            newState[propName] = _transformProp(map, val, valuesMap)
+        }
+    })
+    return newState
+}
+
+
+/**
+ * objMap prop: {storePath:propName, anotherPath:[propName, transformFn], ...}
+ */
+class Connect extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.isFrozen = this.isFrozen.bind(this)
+
+        const self = this
+        this.mounted = false
+        this.timer = null
+
+        this.store = props.__connect.store
+
+        const mapper = props.__connect.transformFn ? props.__connect.transformFn : _mapProps
+
+        let objMap = props.__connect.propsMap
+
+        //allow passing an array of store paths
+        //convert it to obj with key:null pairs
+        if (Array.isArray(objMap)) {
+            const t = {}
+            objMap.forEach(v => {
+                t[v] = null
+            })
+            objMap = t
+        }
+
+        const paths = Object.keys(objMap)
+        const values = this.store.get(paths)
+        const mappedValues = mapper(values, {}, objMap)
+
+        this.state = {
+            mappedProps: mappedValues
+        }
+
+        const {opts, component} = this.props.__connect
+
+        this.unsub = this.store.subscribe(paths, (values, mutations) => {
+                if (!this.mounted) {
+                    return
+                }
+
+                const _setStateAsync = () => {
+                    if (!this.mounted) {
+                        return
+                    }
+                    const newValues = this.store.get(paths)
+                    const mappedMutations = mapper(newValues, mutations, objMap, this.ref)
+//                    console.log('MAPPED', mappedMutations);
+                    this.setState({mappedProps: mappedMutations})
+
+                }
+
+                //window.clearTimeout(this.timer)
+                //this.timer = window.setTimeout(_setStateAsync, 16)
+                scheduler.queue(_setStateAsync)
+
+            },
+            {
+                freeze: this.isFrozen,
+            }
+        )
+
+    }
+
+    isFrozen() {
+        const {opts, component} = this.props.__connect
+        if (opts.freeze && opts.freeze()) {
+
+            // component.name === 'HomeConnected' &&
+//            // console.log('FROZEN', component.name, APP.state);
+            return true
+        }
+
+        // component.name === 'HomeConnected' &&
+//        // console.log('UNFROZEN', component.name, APP.state);
+
+        return false
+    }
+
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.isFrozen()) {
+            return false
+        }
+        return nextState !== this.state
+    }
+
+    // componentWillMount(){}
+    componentDidMount() {
+        this.mounted = true
+    }
+
+    componentWillUnmount() {
+        this.mounted = false
+        if (this.unsub) {
+            this.unsub()
+        }
+    }
+
+    _ref = (ref) => {
+        this.ref = ref;
+
+        //pass the ref further
+        const {__connect, ...rest} = this.props
+        rest.ref && rest.ref(ref)
+    }
+
+    render() {
+
+
+        const {__connect, ...rest} = this.props
+        const MappedComponent = __connect.component
+        const {mappedProps} = this.state
+
+//        // console.log('CONNECT PROPS', MappedComponent.name, mappedProps);
+
+        return (<MappedComponent {...rest} {...mappedProps} ref={this._ref}/>)
+    }
+
+}
+
+/**
+ *  returns a component wrapped in a store paths observer
+ * @param component
+ * @param mapObj
+ * @returns {function(*)}
+ */
+export default function connect(store, component, propsMap, transformFn = null, opts = {}) {
+    return function (props) {
+        return (<Connect  {...props} __connect={{store, component, propsMap, transformFn, opts}}/>)
+    }
+}
+
+export {Connect}
