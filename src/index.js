b2b02d972998cbfc211cc41648115a76d52cfffc
Tudor Ilisoi <tudorilisoi@yahoo.com>
2018-06-17 12:28:31 +0300

    added store merge

--- src/index.js
+++ src/index.js
@@ -4,17 +4,28 @@ import Store from './Store'
 export {connect}
 export {Store}
 
+//TODO move these into the Store classs
+
 function createSetter(store, path) {
     return function (...args) {
         return store.set(path, ...args)
     }
 }
 
+function createMergeSetter(store, path) {
+    return function (...args) {
+        return store.merge(path, ...args)
+    }
+}
+
+
 function createGetter(store, path) {
     return function (...args) {
         return store.get(path, ...args)
     }
 }
+
+//
 
 export function createStore(...args) {
     const store = new Store(...args)
