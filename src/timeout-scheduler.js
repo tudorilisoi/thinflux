53b96999eba207be7433d40c93175bb18f00d502
Tudor Ilisoi <tudorilisoi@yahoo.com>
2019-04-21 21:11:43 +0300

    WIP; perf; product actions; responsive, working, no ticket

--- src/timeout-scheduler.js
+++ src/timeout-scheduler.js
@@ -36,14 +36,14 @@ const tick = uid => {
 
 const queue = (callback, uid) => {
 
-    // InteractionManager.runAfterInteractions(callback)
+    InteractionManager.runAfterInteractions(callback)
 
     // TODO check perf, this is not queued
     // clear(timers[uid])
     // timers[uid] = set(callback, TIMEOUT)
     // return
 
-    callback()
+    // callback()
 
     // tasks.push({callback, uid})
     // if (tasks.length === 1) {
